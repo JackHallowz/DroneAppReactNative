0985a6816c3628c4443b845ac38956b43666c1c7
JackHallowz <kidsaveoursoul@gmail.com>
2023-06-24 09:58:29 +0700

    change

--- AutheProvider.js
+++ AutheProvider.js
@@ -1,1 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
--- SignUp.js
+++ SignUp.js
@@ -4,8 +4,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { TextInput } from 'react-native-gesture-handler';
 import { AntDesign } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
-
+import { Zocial } from '@expo/vector-icons';
+import auth from '@react-native-firebase/auth';
 function SignUp () 
 {   
     const [Email, setEmail] = useState('');
@@ -13,27 +13,42 @@ function SignUp ()
     const [Password, setPassword] = useState('');
     const [Phone, setPhone] = useState('');
 
-    // function signinchange()
-    // {
-
-    // }
+    const  signinchange = () =>
+    {
+        
+        auth().createUserWithEmailAndPassword(Email,Password)
+        .then(()=>{console.log("User `${Email}` has been created");})
+        .catch(error => {
+            if(error.code == 'auth/email-already-in-use'){
+                console.log('that email is already used! Please change your UserName');
+            }
+            if(error.code == 'auth/invalid-email'){
+                console.log('Email Adress is invalid');
+            }
+            console.error(error);
+        }
+            
+        )
+        
+    }
     return(
         <View style={styles.Screen }>
-            <Image source={{uri:'https://static.vecteezy.com/system/resources/previews/002/405/021/original/baby-cats-icon-illustration-vector.jpg'}} resizeMode= 'contain' style={styles.Logo}/>
-            <Text style={styles.TextUserName}> Full Name </Text>
+            <Image source={{uri:'https://previews.123rf.com/images/uo7/uo72207/uo7220700115/189206049-cute-cartoon-little-baby-cat-icon-cat-sleeping-on-the-floor-cat-with-gray-color-cartoon.jpg'}} resizeMode= 'contain' style={styles.Logo}/>
+            {/* <Text style={styles.TextUserName}> Full Name </Text>
             <TextInput style={styles.inputContainer } placeholder='Required' onChangeText={(val)=>setName(val)}></TextInput>
             <Text style={styles.TextUserName}> Phone Number </Text>
-            <TextInput style={styles.inputContainer } placeholder='Optional' numberOfLines={}>
+            <TextInput style={styles.inputContainer } placeholder='Optional' >
                 <AntDesign name="phone" size={18} color="black" />
-            </TextInput>
+            </TextInput> */}
             <Text style={styles.TextUserName}> Email* </Text>
-            <TextInput style={styles.inputContainer} placeholder='Required'>
-                <MaterialIcons name="email" size={18} color="black" />
+            <TextInput style={styles.inputContainer} placeholder='Required' onChangeText={(val)=>setEmail(val)}>
+                <Zocial name="email" size={18} color="black"  />
             </TextInput>
             <Text style={styles.TextUserName}> Password* </Text>
-            <TextInput style={styles.inputContainer} placeholder='less than 20 words' password={true}></TextInput>
+            <TextInput style={styles.inputContainer} placeholder='less than 20 words' secureTextEntry  onChangeText={(val)=>setPassword(val)}>
+            </TextInput>
             <View style={{marginVertical:15,borderRadius: 8, maxWidth: 200, alignSelf:'center'}}>
-                <Button title="Create New Account"  />
+                <Button title="Create New Account" onPress={signinchange} />
             </View>
         </View>
     );
@@ -44,7 +59,6 @@ const styles = StyleSheet.create(
     {
         inputContainer:
         {
-           
             maxHeight:50,
             borderWidth: 1,
             borderColor: '#777',
@@ -65,7 +79,6 @@ const styles = StyleSheet.create(
         Screen:
         {   
             flex:1,
-            
             justifyContent:"center"
         },
         Logo:
